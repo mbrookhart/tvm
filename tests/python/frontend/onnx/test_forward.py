a4a801f65e05e0d568546a9212db20caba2deed5
Matthew Brookhart <mbrookhart@octoml.ai>
2020-08-06 11:55:38 -0700

    Make OneHot dynamic

--- tests/python/frontend/onnx/test_forward.py
+++ tests/python/frontend/onnx/test_forward.py
@@ -703,13 +703,14 @@ def test_gather_nd():
     verify_gather_nd((4, 3, 5, 6), [[2, 1, 0, 0]], 'float32')
 
 
-@tvm.testing.uses_gpu
+# TODO(mbrookhart): enable once VM supports heterogenous execution
+# @tvm.testing.uses_gpu
 def test_onehot():
     indices_shape = [10]
     indices_array = np.random.randint(
         low=0, high=9, size=indices_shape, dtype='int32')
     depth = 10
-    values = np.asarray([0, 1])
+    values = np.asarray([0, 1]).astype("int32")
     out_np = np.eye(depth)[indices_array.reshape(-1)]
 
     onehot_node = helper.make_node(
@@ -723,15 +724,13 @@ def test_onehot():
                                                                     TensorProto.INT32, [1]),
                                       helper.make_tensor_value_info("values",
                                                                     TensorProto.INT32, values.shape)],
-                              initializer=[helper.make_tensor("depth", TensorProto.INT32, [1], [depth]),
-                                           helper.make_tensor("values", TensorProto.INT32, values.shape, values)],
                               outputs=[helper.make_tensor_value_info("out", TensorProto.INT32, out_np.shape)])
 
     model = helper.make_model(graph, producer_name="onehot_test")
 
     for target, ctx in tvm.testing.enabled_targets():
-        tvm_out = get_tvm_output(
-            model, [indices_array], target, ctx, out_np.shape)
+        tvm_out = get_tvm_output_with_vm(
+            model, [indices_array, np.array([depth]).astype("int32"), values], target, ctx)
         tvm.testing.assert_allclose(out_np, tvm_out, rtol=1e-5, atol=1e-5)
 
 
